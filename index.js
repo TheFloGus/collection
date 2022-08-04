512433d170e4f30a45b31f7073b5cc8201c0cb84
TheFloGus <jeka467@gmail.com>
2022-08-04 17:08:17 +0300

    add: more methods

--- index.js
+++ index.js
@@ -18,27 +18,49 @@ class IndexedMap {
       : false;
   }
 
-  get(key){
+  get(key) {
     return this[key] ? this[key] : null;
   }
 
-  remove(key){
-    Object.keys(this).forEach(el => {
-      if(this[el].index > this[key].index)
-      this[el].index -= 1;
+  remove(key) {
+    Object.keys(this).forEach((el) => {
+      if (this[el].index > this[key].index) this[el].index -= 1;
+    });
+    delete this[key];
+    return this;
+  }
+
+  size() {
+    return Object.keys(this).length;
+  }
+
+  uniq() {
+    let values = Object.values(this).map((el) => el.value);
+    let uniqueValues = [...new Set(values)];
+    return uniqueValues;
+  }
+
+  removeAt(index, count = 1){
+    for (let i = 0; i < count; i++){
+      let key = Object.keys(this).find(el => this[el].index === index)
+      delete this[key]
+      index++
+    }
+   
+    Object.keys(this).forEach((el, index) => {
+      this[el].index = index
     })
-    delete this[key]
-    return this
+    return this;
   }
 }
 
 let collection = new IndexedMap();
-collection.set("name", "Eugene");
+collection.set("name0", "Eugene");
 collection.set("name1", "Vika");
 collection.set("name2", "Masha");
 collection.set("name3", "Dima");
 collection.set("name4", "Nate");
 collection.set("name5", "John");
-collection.set("name6", "Luke");
+collection.set("name6", "Eugene");
 collection.set("name7", "Jeane");
-console.log(collection.remove('name1'));
+console.log(collection.removeAt(4, 3));
